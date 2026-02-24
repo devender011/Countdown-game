ec06bd1ea54d12e35d20d6ffdd79b7d4dd7de538
devender011 <dipurajput011@gmail.com>
2026-02-24 22:06:53 +0530

    add name set functionality

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,31 @@
+import { useState,useRef } from "react";
+
 export default function Player() {
+  console.log(useState("devender"))
+  let invalidName=false;
+  const [name, setName] = useState(null);
+  const refName=useRef();
+  const handelClick = () => {
+    if(/^[a-zA-Z0-9]*$/.test(refName.current.value)){
+      setName(refName.current.value);
+      refName.current.value=""
+    }
+    else{
+invalidName=true;
+    }
+  };
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {name??"unkown Entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input
+          ref={refName}
+          type="text"
+          
+        />
+        <button onClick={handelClick}>Set Name</button>
       </p>
+      {invalidName?<p>Enter a valid Name</p>:null}
     </section>
   );
 }
